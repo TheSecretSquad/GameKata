90f64fb2f55cfb3c4cc75647a09a8795ce562d15
James Ladd <ladd.james@gmail.com>
2017-11-12 10:04:09 +1100

    Initial commit of starter test.

---
+++ src/main/java/com/thingo/game/impl/TicTacToe.java
@@ -0,0 +1,11 @@
+package com.thingo.game.impl;
+
+import com.thingo.game.Game;
+
+public class TicTacToe implements Game {
+
+    @Override
+    public Game start() {
+        return this;
+    }
+}
---
+++ src/test/java/com/thingo/game/impl/TicTacToeTest.java
@@ -0,0 +1,28 @@
+package com.thingo.game.impl;
+
+import com.thingo.game.Game;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TicTacToeTest {
+
+    // List the behaviour we want from Game
+    // then take each behaviour and implement it.
+
+    // Read the items below as "The Class Under Test 'Game' ..."
+
+    // shouldBeStartable
+    // shouldBeStopable
+
+    @Test
+    public void shouldBeStartable() {
+        Game game = new TicTacToe();
+        game = game.start();
+        assertGameHasStated(game);
+    }
+
+    private void assertGameHasStated(Game game) {
+        assertTrue("Game not started", false);
+    }
+}
